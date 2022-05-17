22659cc0021f79348989d71b7e1848b30a9e4851
Christopher Quiroz <cquirozmendivel@gmail.com>
2022-05-17 17:57:39 +0200

    add url send email ajax

--- PortfolioWebApp/urls.py
+++ PortfolioWebApp/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from PortfolioWebApp import views
-
 urlpatterns = [
     path('', views.home, name="Inicio"),
     path('sobre-mi', views.sobre_mi, name="sobre-mi"),
     path('proyectos', views.proyectos, name="proyectos"),
     path('contacto', views.contacto, name="contacto"),
+    path('sendform', views.send_form, name="sendform"),
 ] 
 
